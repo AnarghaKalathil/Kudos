34e449637f8b7bca566035e3f66de55db7950c13
sreeshanth shimji <sreeshanth.shimji@terrificminds.com>
2025-07-23 18:29:28 +0530

    Blah

--- app-backend/kudosbackend/accounts/urls.py
+++ app-backend/kudosbackend/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls.conf import path
 
-from .views import LoginApiView, LogoutView
+from .views import LoginApiView, LogoutView, SaveFCMToken
 
 app_name = "accounts"
 
 urlpatterns = [
     path("api/login/", LoginApiView.as_view(), name="login"),
     path("api/logout/", LogoutView.as_view(), name="logout"),
+    path("api/device-token/", SaveFCMToken.as_view(), name="device-token"),
 ]
