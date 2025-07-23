9a8f07f52660adda89fb063d73ba5195059ef7c1
sreeshanth shimji <sreeshanth.shimji@terrificminds.com>
2025-07-23 14:19:10 +0530

    Blah

--- app-backend/kudosbackend/dashboard/serializer.py
+++ app-backend/kudosbackend/dashboard/serializer.py
@@ -17,10 +17,10 @@ class RecognitionListSerializer(serializers.ModelSerializer):
                   "reviewer", "skills", "status"]
 
     def get_sender(self, obj):
-        return obj.sender.username
+        return obj.sender.username if obj.sender.username  else None
 
     def get_reviewer(self, obj):
-        return obj.reviewer.username
+        return obj.reviewer.username if obj.reviewer.username else None
 
     def get_skills(self, obj):
         return obj.skills.all().values_list("name", flat=True)
