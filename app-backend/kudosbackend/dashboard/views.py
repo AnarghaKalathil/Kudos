2a8f84ae9c9086110f9e6fa0316761d22b593f05
sreeshanth shimji <sreeshanth.shimji@terrificminds.com>
2025-07-24 20:10:25 +0530

    Blah

--- app-backend/kudosbackend/dashboard/views.py
+++ app-backend/kudosbackend/dashboard/views.py
@@ -77,7 +77,8 @@ class Dashboard(APIView):
             total_users = KudosUser.objects.filter(is_superuser=False).count()
 
             total_contributors = KudosUser.objects.filter(
-                recognitions_given__isnull=False
+                recognitions_given__isnull=False,
+                recognitions_given__status=RecognitionStatus.APPROVED
             ).distinct().count()
 
             total_star_count = Star.objects.count()
